fe57a7beae6b33d916ad26f79f4f723d627850b6
Kodmindset <kodemindset@gmail.com>
2022-12-18 11:01:49 +0000

    main files eddited

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-
 window.onload = function(){
     
     let NumOfScore= JSON.parse(localStorage.getItem('score'));
@@ -6,6 +5,7 @@ window.onload = function(){
      
      const stagehands = document.querySelector(".hands");
      const stagebonus = document.querySelector(".bonus");
+    
      const showDefault =0;
 
 
@@ -22,9 +22,30 @@ window.onload = function(){
     else{
         stagebonus.style.display="none";
         stagehands.style.display="flex";
-    }
+    };
+    let rules = document.querySelector(".rules");
+   
+   
+    //let rules =document.querySelector(".rules");
+    
+    $(document).mouseup(function (e) {
+        if ($(e.target).closest(".showRuleImage").length === 0) {
+            $(".showRuleImage").hide();
+            $(".ShowRules").css("display","block");
+           
+        }
+      
+
+    });
+   
+    
+   
+    
+
+
    
 }
+
 
 let NumOfScore= JSON.parse(localStorage.getItem('score'));
 
@@ -416,18 +437,23 @@ const RulesOnClick =()=>{
     
      let contest = document.querySelector(".contest");
      contest.style.display = "none";
-    const stagehands = document.querySelector(".hands");
+     let ShowRules = document.querySelector(".ShowRules");
+     ShowRules.style.display = "none";
+    /*const stagehands = document.querySelector(".hands");
     const stagebonus = document.querySelector(".bonus");
     stagehands.style.display = "none";
-    stagebonus.style.display = "none";
+    stagebonus.style.display = "none"; */
+
      /*show the Rules page when clicked*/
     const showImage =document.querySelector(".showRuleImage");
-    showImage.style.display="flex";
-    
-   
 
-    const ShowRules =document.querySelector(".ShowRules");
-    ShowRules.style.display="none";
+    showImage.style.display="grid";
+
+    const close =document.querySelector(".close");
+    close.style.display="none";
+
+    
+    
 }
 const CloseOnClick =()=>{
      /*hind the current page*/
@@ -522,8 +548,4 @@ const saveToLocalMemory =()=>{
     
     // now save oldd data + new data to the local storage
     score = localStorage.setItem('data',JSON.stringify(old_data));
-
    document.querySelector(".score h1").innerText = score;*/
-
-
-   
